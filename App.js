3b1ae28f247f00ea25a307b747ae926cbcce8116
Bruno Maciel <151591698+BrunuMac@users.noreply.github.com>
2024-08-25 12:43:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>Ola socorro</Text>
+      <StatusBar style='auto' hidden={false} translucent={false} barStyle='default'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'start',
+  },
+
+  txt: {
+    fontSize: 50
+  },
+
+});
